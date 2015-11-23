2a983a97f4088d8498c5d2e8a94f3e42bf0a62be
tanzm <Tanzila.Mohammad@fao.org>
2015-11-23 15:13:53 +0100

    updates

---
+++ i18n/root/analyze-modules.js
@@ -0,0 +1,17 @@
+/*global define*/
+define({
+    title: "Analyze Data",
+    text: "Change me in 118n/root/modules.js",
+    modules:[
+        {
+            "title": {
+                "EN": "Compare"
+            },
+            "body": {
+                "EN": "Compare data."
+            },
+            "link": "#analyze/compare",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/crowdprices/crowdprices.png"
+        }
+    ]
+});
---
+++ src/js/views/analyze-view.js
@@ -0,0 +1,257 @@
+/*global define, amplify*/
+define([
+    'views/base/view',
+    'fx-ds/start',
+    'fx-filter/start',
+    'text!templates/analyze/analyze.hbs',
+    'text!templates/analyze/dashboard.hbs',
+    'text!templates/common/modules.hbs',
+    'i18n!nls/analyze',
+    'i18n!nls/topics-flude',
+    'i18n!nls/analyze-modules',
+    'config/Events',
+    'config/Config',
+    'text!config/browse/flude-topics.json',
+    'config/analyze/config',
+    'fx-filter/Fx-filter-configuration-creator',
+    'handlebars',
+    'lib/utils',
+    'amplify',
+    'select2',
+    'jstree',
+    'highcharts-export'
+], function (View, Dashboard, Filter, template, browseByDashboardTemplate, modulesTemplate, i18nLabels, topicFludeLabels, moduleLabels, E, C, FludeTopics, TopicFludeConfig, FilterConfCreator, Handlebars, Utils) {
+
+    'use strict';
+
+    var s = {
+        TOPIC_SELECTOR_FLUDE: "#flude-topic-selector",
+        TOPIC_CONTENT_FLUDE: "#flude-topic-content",
+        FILTER_OPENER_FLUDE: ".filter-opener-flude",
+        FILTER_CONTAINER_FLUDE: "#filter-container-flude",
+        FILTER_FLUDE: "filter-flude",
+        FILTER_SUBMIT_FLUDE: "#filter-submit-btn-flude",
+
+        SIDE_FLUDE: "#side-flude",
+
+
+        DASHBOARD_FLUDE_CONTAINER: '#dashboard-flude-container'
+
+
+    };
+
+    var AnalyzeView = View.extend({
+
+        // Automatically render after initialize
+        autoRender: true,
+
+        className: 'analysis',
+
+        // Save the template string in a prototype property.
+        // This is overwritten with the compiled template function.
+        // In the end you might want to used precompiled templates.
+        template: template,
+
+        initialize: function (params) {
+            this.analyze_type = params.filter;
+            this.page = params.page;
+
+            View.prototype.initialize.call(this, arguments);
+        },
+
+        getTemplateData: function () {
+          return i18nLabels;
+         },
+
+        attach: function () {
+
+            View.prototype.attach.call(this, arguments);
+
+            //update State
+            amplify.publish(E.STATE_CHANGE, {menu: 'analyze', breadcrumb: this._initMenuBreadcrumbItem()});
+
+            this._initVariables();
+
+            this._initComponents();
+
+            this._bindEventListeners();
+
+            this.analyze_type ? this._showFludeTopic(this.analyze_type) :  this._displayAnalyzeOptions() ;
+        },
+
+        _initMenuBreadcrumbItem: function() {
+            var label = "";
+            var self = this;
+
+            if (typeof self.analyze_type !== 'undefined') {
+                label = i18nLabels[self.analyze_type];
+            }
+
+            return Utils.createMenuBreadcrumbItem(label, self.analyze_type, self.page);
+        },
+
+        _initVariables: function () {
+
+            this.$topicSelectorFlude = this.$el.find(s.TOPIC_SELECTOR_FLUDE);
+            this.$topicContentFlude = this.$el.find(s.TOPIC_CONTENT_FLUDE);
+
+            this.$filterOpenerFlude = this.$el.find(s.FILTER_OPENER_FLUDE);
+            this.$filterContainerFlude = this.$el.find(s.FILTER_CONTAINER_FLUDE);
+
+            this.$filterSubmitFlude = this.$el.find(s.FILTER_SUBMIT_FLUDE);
+
+            this.$sideFlude = this.$el.find(s.SIDE_FLUDE);
+
+
+
+        },
+
+        _bindEventListeners: function () {
+
+            var self = this;
+
+            this.$topicSelectorFlude.on("change", function (e) {
+                self._onFludeTopicChange(e.val);
+            });
+
+            this.$filterSubmitFlude.on('click', function (e, data) {
+
+                var filter = {};
+                var values = self.filterFlude.getValues();
+                // TODO: funzione per distruggere dashboard e ricrearla con gli items giusti:
+                /*
+                 var filteredConfig = self._getFilteredConfig(values, self.$faostatDashboardConfig);
+                 self._renderFaostatDashboard(filteredConfig);
+                 self.fludeDashboard.filter([values]);
+                 */
+
+                // TODO: it's an array
+                self.fludeDashboard.filter([values]);
+            });
+
+        },
+
+        _onFludeTopicChange: function (topic) {
+            this._showFludeTopic(topic);
+        },
+
+
+
+        _showFludeTopic: function (topic) {
+           console.log("_showFludeTopic topric = "+topic);
+
+            var self = this;
+
+            //Inject HTML
+           var source = $(browseByDashboardTemplate).find("[data-topic='" + topic + "']"),
+               template = Handlebars.compile(source.prop('outerHTML')),
+               html = template(topicFludeLabels[topic]);
+
+            this.$topicContentFlude.html(html);
+
+            this._renderFludeComponents(topic);
+
+        },
+
+
+        _initComponents: function () {
+
+            //Flude
+
+            var conf = JSON.parse(FludeTopics);
+
+            this.$topicSelectorFlude.select2(conf);
+
+            this.$topicSelectorFlude.select2('data', conf.data[0]);
+           // this._onFludeTopicChange(conf.data[0].id);  // sets first item as default
+
+
+        },
+
+        _renderFludeComponents: function (topic) {
+            console.log("_renderFludeComponents topric = "+topic);
+            var config = TopicFludeConfig[topic];
+
+            if (!config || !config.dashboard || !config.filter) {
+                alert(" HERE Impossible to find configuration for topic: " + topic);
+                return;
+            }
+
+            var filterConfig = config.filter;
+
+            console.log(filterConfig);
+
+            this._renderFludeFilter(filterConfig);
+
+            console.log(config.dashboard);
+
+            this._renderFludeDashboard(config.dashboard);
+
+
+        },
+
+        _updateDashboardTitles : function (filter) {
+
+            //console.log(filter)
+
+
+
+        },
+
+
+        _renderFludeDashboard: function (config) {
+
+            if (this.fludeDashboard && this.fludeDashboard.destroy) {
+                this.fludeDashboard.destroy();
+            }
+
+            console.log(config);
+
+            this.fludeDashboard = new Dashboard({
+
+                //Ignored if layout = injected
+                container: s.DASHBOARD_FLUDE_CONTAINER,
+                layout: "injected"
+            });
+
+            this.fludeDashboard.render(config);
+
+        },
+
+        _renderFludeFilter: function (config) {
+
+            var self = this;
+
+            this.filterConfCreator = new FilterConfCreator();
+
+            this.filterConfCreator.getConfiguration(config)
+                .then(function (c) {
+
+                    self.filterFlude = new Filter();
+
+                    self.filterFlude.init({
+                        container: s.FILTER_FLUDE,
+                        layout: 'fluidGrid'
+                    });
+
+                    var adapterMap = {};
+
+                    self.filterFlude.add(c, adapterMap);
+
+                });
+
+        },
+
+        _displayAnalyzeOptions: function () {
+            var template = Handlebars.compile(modulesTemplate),
+                html = template({modules: moduleLabels["modules"]});
+
+           this.$el.html(html);
+        }
+
+
+
+    });
+
+    return AnalyzeView;
+});
