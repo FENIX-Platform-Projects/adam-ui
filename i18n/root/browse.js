fd3277244fc1ef078e7c6c552cea603e92741332
tanzm <Tanzila.Mohammad@fao.org>
2015-12-03 17:19:05 +0100

    ADAM

--- i18n/root/browse.js
+++ i18n/root/browse.js
@@ -5,5 +5,9 @@ define({
     "filter" : "Filter Data",
     "text" : "Change me in 118n/root/analysis.js",
     "lateral_menu_flude_title" : "Topics",
-    "filter_btn" : "Filter"
+    "filter_btn" : "Filter",
+    "sector": "By Sector",
+    "country_sector": "By Country - Sector",
+    "donor_sector": "By Donor - Sector",
+    "country_donor_sector": "By Country - Donor - Sector"
 });
