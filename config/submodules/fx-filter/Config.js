c5774ae0dda4ad488c047a81bc41962854e48f67
tanzm <Tanzila.Mohammad@fao.org>
2015-12-12 22:57:58 +0100

    adam

--- config/submodules/fx-filter/Config.js
+++ config/submodules/fx-filter/Config.js
@@ -7,11 +7,19 @@ define([
     'use strict';
 
     var SERVER = C.SERVER;
+    var SERVER_POSTFIX = C.CODELIST_POSTFIX;
+    var SERVICE = C.CODELIST_SERVICE;
 
     return {
 
         SERVER :SERVER,
-        SERVICE_BASE_ADDRESS: SERVER + "d3s_dev/msd"
-
+        //SERVICE_BASE_ADDRESS: SERVER + "d3s_dev/msd",
+        SERVICE_POSTFIX: SERVER_POSTFIX,
+        SERVICE_BASE_ADDRESS: SERVER + SERVICE,
+        D3S_METADATA_URL: SERVER + SERVICE + "/resources/",
+        D3S_CODELIST_URL: SERVER + SERVICE+SERVER_POSTFIX,
+        D3S_FILTER_CODES : SERVER + SERVICE + "/codes/filter",
+        OVERWRITE_DEFAULT_CONFIG : true ,
+        lang: "EN"
     };
 });
