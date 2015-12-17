2c9eb98f253f4c855f0dd035943f0c3140af9c82
tanzm <Tanzila.Mohammad@fao.org>
2015-12-17 16:49:31 +0100

    adam

--- i18n/root/analyze-modules.js
+++ i18n/root/analyze-modules.js
@@ -1,17 +1,72 @@
 /*global define*/
 define({
-    title: "Analyze Data",
+    title: "Analyse Data",
     text: "Change me in 118n/root/modules.js",
     modules:[
         {
+            "header": {
+                "EN": "Analyse data"
+            },
             "title": {
                 "EN": "Compare"
             },
             "body": {
-                "EN": "Compare data."
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
             },
             "link": "#analyze/compare",
             "img": "http://fenixrepo.fao.org/cdn/images/modules/crowdprices/crowdprices.png"
+        },
+        {
+            "header": {
+                "EN": "Analyse data"
+            },
+            "title": {
+                "EN": "Implementing Agencies"
+            },
+            "body": {
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+            },
+            "link": "#analyze/agencies",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/trade_AFG/trade_AFG.png"
+        },
+        {
+            "header": {
+                "EN": "Analyse data"
+            },
+            "title": {
+                "EN": "Donor Matrix"
+            },
+            "body": {
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+            },
+            "link": "#analyze/donor_matrix",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/env_AFG/environmental_AFG.png"
+        },
+        {
+            "header": {
+                "EN": "Analyse data"
+            },
+            "title": {
+                "EN": "Project Analysis"
+            },
+            "body": {
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.."
+            },
+            "link": "#analyse/sector",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/kenya/timeseries/kenya_monitoring_timeseries.png"
+        },
+        {
+            "header": {
+                "EN": "Analyse data"
+            },
+            "title": {
+                "EN": "Priority Analysis"
+            },
+            "body": {
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.."
+            },
+            "link": "#analyse/priority",
+            "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/kenya/timeseries/kenya_monitoring_timeseries.png"
         }
     ]
 });
--- i18n/root/browse-modules.js
+++ i18n/root/browse-modules.js
@@ -1,44 +1,56 @@
 /*global define*/
 define({
-    title: "Browse Data",
+    title: "Browse by",
     text: "Change me in 118n/root/modules.js",
     modules:[
         {
+            "header": {
+                "EN": "Browse by"
+            },
             "title": {
-                "EN": "Browse Data By Country - Sector"
+                "EN": "Country - Sector"
             },
             "body": {
-                "EN": "Analysis of the trade flows at regional and national levels and for comparing the behaviors of regional and international import and export dynamics over time."
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
             },
             "link": "#browse/country_sector",
             "img": "http://fenixrepo.fao.org/cdn/images/modules/crowdprices/crowdprices.png"
         },
         {
+            "header": {
+                "EN": "Browse by"
+            },
             "title": {
-                "EN": "Browse Data By Donor - Sector"
+                "EN": "Donor - Sector"
             },
             "body": {
-                "EN": "Analysis of the trade flows at regional and national levels and for comparing the behaviors of regional and international import and export dynamics over time."
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
             },
             "link": "#browse/donor_sector",
             "img": "http://fenixrepo.fao.org/cdn/images/modules/trade_AFG/trade_AFG.png"
         },
         {
+            "header": {
+                "EN": "Browse by"
+            },
             "title": {
-                "EN": "Browse Data By Country - Donor - Sector"
+                "EN": "Country - Donor - Sector"
             },
             "body": {
-                "EN": "Analysis of the trade flows at regional and national levels and for comparing the behaviors of regional and international import and export dynamics over time."
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
             },
             "link": "#browse/country_donor_sector",
             "img": "http://fenixrepo.fao.org/cdn/images/modules/env_AFG/environmental_AFG.png"
         },
         {
+            "header": {
+                "EN": "Browse by"
+            },
             "title": {
-                "EN": "Browse Data By Sector"
+                "EN": "Sector"
             },
             "body": {
-                "EN": "Analysis of the trade flows at regional and national levels and for comparing the behaviors of regional and international import and export dynamics over time."
+                "EN": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
             },
             "link": "#browse/sector",
             "img": "http://fenixrepo.fao.org/cdn/images/modules/monitoring/kenya/timeseries/kenya_monitoring_timeseries.png"
