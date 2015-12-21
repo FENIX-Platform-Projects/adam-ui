754fc2efa18c66738ae321b964b11f70955ac09f
tanzm <Tanzila.Mohammad@fao.org>
2015-12-21 16:26:50 +0100

    adam

--- config/Config.js
+++ config/Config.js
@@ -22,7 +22,7 @@ define(function () {
         TOP_MENU_CONFIG: 'config/submodules/fx-menu/top_menu.json',
         TOP_MENU_TEMPLATE: 'fx-menu/templates/blank-fluid.html',
         TOP_MENU_SHOW_BREADCRUMB: true,
-        TOP_MENU_SHOW_BREADCRUMB_HOME: true,
+        TOP_MENU_SHOW_BREADCRUMB_HOME: false,
         TOP_MENU_SHOW_FOOTER: false,
         TOP_MENU_AUTH_MODE_HIDDEN_ITEMS: ['login'],
         TOP_MENU_PUBLIC_MODE_HIDDEN_ITEMS: ['datamgmt', 'logout'],
