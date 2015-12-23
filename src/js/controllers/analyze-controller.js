c6228664efc51e77e8e368a6e408f3283a5a1160
tanzm <Tanzila.Mohammad@fao.org>
2015-12-23 11:36:48 +0100

    adam

--- src/js/controllers/analyze-controller.js
+++ src/js/controllers/analyze-controller.js
@@ -1,16 +1,24 @@
 /*global define*/
 define([
+    'chaplin',
     'controllers/base/controller',
+    'views/analyze-view',
     'views/analyze/compare/compare-view'
-], function (Controller, View) {
+], function (Chaplin, Controller, AnalyzeView, CompareView) {
 
     'use strict';
 
     var AnalyzeController = Controller.extend({
 
         show: function (params) {
+           this.view = new AnalyzeView({
+                region: 'main',
+                page: Backbone.history.fragment
+            });
+        },
 
-            this.view = new View({
+        compare: function (params) {
+            this.view = new CompareView({
                 region: 'main',
                 filter: params.filter
             });
