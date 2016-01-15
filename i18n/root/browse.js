f99af4c165da41e3d2d1e41276c6dd9db1028455
tanzm <Tanzila.Mohammad@fao.org>
2016-01-15 13:34:50 +0100

    adam

--- i18n/root/browse.js
+++ i18n/root/browse.js
@@ -1,9 +1,7 @@
 /*global define*/
 define({
     "title": "Browse By",
-    "filter" : "Filter Data",
     "text" : "Change me in 118n/root/browse.js",
-    "filter_btn" : "Update",
     "sector": "By Sector",
     "country_sector": "By Country & Sector",
     "donor_sector": "By Donor & Sector",
