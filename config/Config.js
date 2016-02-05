5494a08a997b3e9dc7b203dc9f7e4da86a11815b
Daniele Salvatore <daniele.salvatore@fao.org>
2016-02-05 14:00:30 +0100

    BUG FIX: compare getResouce URL was broken

--- config/Config.js
+++ config/Config.js
@@ -10,7 +10,7 @@ define(function () {
 
         SERVER: SERVER,
         CODES_POSTFIX : '/codes/filter',
-        D3P_POSTFIX : "/processes/",
+        D3P_POSTFIX : "d3s/processes/",
 
         //Chaplin JS configuration
         CHAPLINJS_CONTROLLER_SUFFIX: '-controller',
