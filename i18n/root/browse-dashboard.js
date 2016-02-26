660057fb6d277805e1e47ece381cbd87656df07d
tanzm <Tanzila.Mohammad@fao.org>
2016-02-26 14:10:38 +0000

    adam

---
+++ i18n/root/browse-dashboard.js
@@ -0,0 +1,15 @@
+/*global define*/
+define({
+    title: "Dashboard Items",
+    text: "Change me in 118n/root/modules.js",
+    "tot-oda": "Total ODA",
+    "top-partners": "Top Resource Partners",
+    "top-recipients": "Top Recipients",
+    "top-sectors": "Top Sectors",
+    "top-subsectors": "Top Sub-Sectors",
+    "top-channels": "Top Channels of Delivery",
+    "oda-regional": "ODA Regional Distribution",
+    "country-map": "Regional Map on ODA received",
+    "donor_country-map": "Top Recipients"
+
+});
