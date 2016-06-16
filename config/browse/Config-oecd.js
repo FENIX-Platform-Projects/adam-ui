7567a065672731e9bbc9b0d76795a4063c1ef1d4
stefanocudini <stefano.cudini@gmail.com>
2016-06-16 17:23:10 +0200

    test country config bashboard

--- config/browse/Config-oecd.js
+++ config/browse/Config-oecd.js
@@ -320,7 +320,57 @@ define(function () {
 
                 ]
             }
+        },
+/*
+        "country": {
+            dashboard: {
+                //default dataset id
+                uid: "browse-country",
+
+                items: [
+                    {
+                        id: 'country-map',
+                        type: 'map',
+                        config: {
+                            geoSubject: 'gaul0',
+                            colorRamp: 'Reds',  //Blues, Greens, 
+                            //colorRamp values: http://fenixrepo.fao.org/cdn/fenix/fenix-ui-map-datasets/colorramp.png
+                            fenix_ui_map: {
+
+                                guiController: {
+                                    overlay: false,
+                                    baselayer: false,
+                                    wmsLoader: false
+                                },
+
+                                baselayers: {
+                                    "cartodb": {
+                                        title_en: "CartoDB light",
+                                        url: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
+                                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+                                        subdomains: 'abcd',
+                                        maxZoom: 19
+                                        // title_en: "Baselayer",
+                                        // url: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
+                                        // subdomains: 'abcd',
+                                        // maxZoom: 19
+                                    }
+                                },
+                                labels: true,
+                                boundaries: true
+                                //highlightCountry: ['TCD','MLI','NER']
+                            }
+                        },
+
+                        filter: { //FX-filter format
+                            parentsector_code: ["600"],
+                           // year: [{value: 2000, parent: 'from'}, {value: 2014, parent:  'to'}]
+                        }
+                    }
+                ]
+            }
         }
+*/
     }
 
 
