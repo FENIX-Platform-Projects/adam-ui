f9904ea54901215210669003f65d57c615c3330e
tanzm <Tanzila.Mohammad@fao.org>
2016-07-13 10:31:04 +0100

    ADAM updates

--- config/submodules/fx-chart/highcharts_template.js
+++ config/submodules/fx-chart/highcharts_template.js
@@ -6,10 +6,28 @@ define(function () {
     return {
 
         chart: {
-            spacing: [10, 10, 15, 10], // better spacing when chart exports
+            spacing: [10, 10, 27, 10], // better spacing when chart exports
 
             events: {
                 load: function (event) {
+
+
+                    Highcharts.setOptions({
+                        lang: {
+                            toggleDataLabels: 'Display/hide values on the chart',
+                            download: 'Download chart options'
+                        }
+                    });
+
+                    if(this.series.length < 2){
+                        $.each(this.series, function (i, serie) {
+                               serie.update({
+                                    showInLegend: false
+                                })
+                        });
+                        //this.redraw();
+                    }
+
                     if (this.options.chart.forExport) {
                         this.xAxis[0].update({
                             categories: this.xAxis[0].categories,
@@ -37,7 +55,29 @@ define(function () {
                             }, false);
                         });
 
-                        Highcharts.each(this.series, function (series) {
+                        $.each(this.series, function (i, serie) {
+                            if(!serie.visible){
+                                serie.update({
+                                    showInLegend: false
+                                })
+                            } else {
+                                if(serie.options.dataLabels.enabled){
+                                         serie.update({
+                                          marker : {
+                                             radius: 2
+                                         },
+                                         dataLabels: {
+                                           enabled: true,
+                                          style: {
+                                            fontSize: '6px'
+                                         }
+                                         }
+                                     })
+                                }
+                           }
+                        });
+
+                      /**  Highcharts.each(this.series, function (series) {
                             series.update({
                                 marker : {
                                     radius: 2
@@ -51,9 +91,99 @@ define(function () {
                                     }
                                 }
                             }, false);
+                        });**/
+                        this.redraw();
+                    }
+
+
+                },
+                beforePrint: function (event) {
+
+
+                    Highcharts.setOptions({
+                        lang: {
+                            toggleDataLabels: 'Display/hide values on the chart',
+                            download: 'Download chart options'
+                        }
+                    });
+
+                    if(this.series.length < 2){
+                        $.each(this.series, function (i, serie) {
+                            serie.update({
+                                showInLegend: false
+                            })
                         });
                         this.redraw();
                     }
+
+                    if (this.options.chart.forExport) {
+                        this.xAxis[0].update({
+                            categories: this.xAxis[0].categories,
+                            labels: {
+                                style: {
+                                    width: '50px',
+                                    fontSize: '6px'
+                                },
+                                step: 1
+                            }
+                        }, false);
+
+                        Highcharts.each(this.yAxis, function (y) {
+                            y.update({
+                                title: {
+                                    style: {
+                                        fontSize: '6px'
+                                    }
+                                },
+                                labels: {
+                                    style: {
+                                        fontSize: '6px'
+                                    }
+                                }
+                            }, false);
+                        });
+
+                        $.each(this.series, function (i, serie) {
+                            if(!serie.visible){
+                                serie.update({
+                                    showInLegend: false
+                                })
+                            } else {
+                                if(serie.options.dataLabels.enabled){
+                                    serie.update({
+                                        marker : {
+                                            radius: 2
+                                        },
+                                        dataLabels: {
+                                            enabled: true,
+                                            style: {
+                                                fontSize: '6px'
+                                            }
+                                        }
+                                    })
+                                }
+                            }
+                        });
+
+                        /**  Highcharts.each(this.series, function (series) {
+                            series.update({
+                                marker : {
+                                    radius: 2
+                                },
+                                dataLabels: {
+                                    enabled: true,
+                                    style: {
+                                        fontSize: '6px',
+                                        color: series.color,
+                                        textShadow: 0
+                                    }
+                                }
+                            }, false);
+                        });**/
+                        this.redraw();
+                    }
+
+
                 }
             }
 
@@ -63,9 +193,9 @@ define(function () {
             enabled: true,
             position: {
                 align: 'left',
-                x: 10
+                x: 5
             },
-            text: 'Source: OECD',
+            text: 'Source: OECD-CRS',
             href: ''
         },
 
@@ -74,9 +204,11 @@ define(function () {
             sourceWidth: 700,
             buttons: {
                 contextButton: {
-                    _titleKey: "doptions",
-                    text: "Download"//,
-                    // menuItems: [{
+
+                    text: "Download",
+                    _titleKey: "download"
+
+                    //, menuItems: [{
                     //    textKey: 'downloadPNG',
                     //    onclick: function () {
                     //        this.exportChart();
@@ -89,7 +221,53 @@ define(function () {
                     //    });
                     // }
                     // }]
-                }
+                },
+                toggleDataLabelsButton: {
+                    text: "Display Values",
+                    _titleKey: "toggleDataLabels",
+                    onclick: function (){
+
+                        var button = this.exportSVGElements[2],
+                            $button = $(button.element.lastChild),
+                        text = $button.text() == "Display Values" ? "Hide Values" : "Display Values";
+
+                        button.attr({
+                          text: text
+                        });
+
+                        for(var idx = 0; idx < this.series.length; idx++){
+                            var opt = this.series[idx].options;
+                            var isShown = !opt.dataLabels.enabled;
+                            this.series[idx].update({dataLabels: {enabled: isShown,  style: {
+                               // fontSize: '7px',
+                                color: this.series[idx].color,
+                                textShadow: 0
+                            }}});
+                        }
+
+                    }
+                }/**,
+                printButton: {
+                     text: "Print",
+                   // _titleKey: "toggleDataLabels",
+                    onclick: function (){
+                        var chart = $(this.renderTo).highcharts();
+                        //console.lo
+                        $(this.renderTo).find('.highcharts-button').hide();
+                        $(this.renderTo).find('.highcharts-subtitle').hide();
+                        $(this.renderTo).find('.highcharts-legend-title').hide();
+
+                        chart.print();
+
+                        $(this.renderTo).find('.highcharts-button').show();
+                        $(this.renderTo).find('.highcharts-subtitle').show();
+                        $(this.renderTo).find('.highcharts-legend-title').show();
+                        //alert(this.renderTo)
+                        //this.setTitle("Hello");
+
+                       // this.print();
+                    }
+                }**/
             },
 
 
@@ -124,12 +302,15 @@ define(function () {
                 subtitle: {
                     style: {
                         fontSize: '8px'
-                    }
+                    },
+                    align: 'center'
                 },
                 credits: {
                     style: {
-                        fontSize: '6px'
+                        fontSize: '6px',
+                        margin: '30px'
                     }
+
                 },
                 legend: {
                     title: {
@@ -139,14 +320,27 @@ define(function () {
                         return '<span style="color:'+this.color+'">'+this.name+'</span>';
                     },
                     itemStyle: {
-                        fontSize: '6px',
-                        fontWeight: 'bold'
+                        fontSize: '6px'//,
+                        //fontWeight: 'bold'
                     },
                     enabled: false//, only one series and all info in title and subtitle
                 },
                 plotOptions: {
                     series: {
                         lineWidth: 1
+                    }
+                },
+                series: {
+                   // marker : {
+                   //     radius: 2
+                  // },
+                    dataLabels: {
+                        enabled: false
+                       // style: {
+                           // fontSize: '6px',
+                           // color: this.series.color,
+                           // textShadow: 0
+                       // }
                     }
                 }
 
@@ -176,7 +370,8 @@ define(function () {
 
         subtitle: {
             text: '<b>Hover for values and click and drag to zoom</b>',
-            x: -20
+            align: 'left',
+            x: 10
         },
 
         yAxis: [{ //Primary Axis
