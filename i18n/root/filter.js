8ff1cafd65aff20c64a7cd8be8dda0f3f55c1f0d
tanzm <Tanzila.Mohammad@fao.org>
2016-08-29 15:51:48 +0100

    New description for ODA

--- i18n/root/filter.js
+++ i18n/root/filter.js
@@ -11,5 +11,6 @@ define({
     "filter_year-to": "To",
     "filter_uid": "ODA",
     "filter_oda": "ODA",
-    "filter_tooltip_oda": "Official Development Assistance (ODA) - Grants, Grant-Like, Loans and Equity Investment"
+    "filter_tooltip_old": "Official Development Assistance (ODA) - Grants, Grant-Like, Loans and Equity Investment",
+    "filter_tooltip_oda": "The ODA data shown in the chart is on money that flows out of donor countries, not necessarily the money that flows into recipient countries. There are multiple reasons why these two might not match up as lots of ODA is actually spent in the donor country (but still qualifies as ODA, if the economic welfare of a developing country is the ‘main’ aim). For example, some of it goes towards debt relief, or student costs in the donor country. Where not specifically mentioned, ODA refers to CRS data."
 });
