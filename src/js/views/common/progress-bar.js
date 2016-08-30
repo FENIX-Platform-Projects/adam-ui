81b9655de6c45e36804cc07b4023d5a2cbbcbe1d
tanzm <Tanzila.Mohammad@fao.org>
2016-08-30 14:47:39 +0100

    Progress Bar added

--- src/js/views/common/progress-bar.js
+++ src/js/views/common/progress-bar.js
@@ -4,39 +4,48 @@ define([
     "jquery",
     "loglevel",
     'underscore',
+    'text!templates/common/progress-bar.hbs',
     'lib/utils',
-    'i18n!nls/browse',
+    'i18n!nls/common',
     'handlebars',
     'amplify'
-], function ($, log, _, Utils, i18nLabels, Handlebars) {
+], function ($, log, _, template, Utils, i18nLabels, Handlebars) {
 
     'use strict';
 
+    var defaultOptions = {
+        id: '#progress-bar'
+    };
 
     function ProgressBar(o) {
 
         var self = this;
 
-        $.extend(true, this, {}, o);
-        this.$el = $(this.el);
+        $.extend(true, this, defaultOptions, o);
+        this.$container = $(this.container);
 
+        this._renderTemplate();
+
+        this._render();
 
         return this;
     }
 
-    ProgressBar.prototype.update = function (value) {
+    ProgressBar.prototype._renderTemplate = function () {
 
-        var $div = this.$el.find('div');
-
-        var $span = $div.find('span');
-
-        $div.attr('aria-valuenow', value);
-        $div.css('width', value + '%');
-        $span.text(value + '% Complete');
+        this.progressBar = Handlebars.compile(template);
 
     };
 
+    ProgressBar.prototype._render = function () {
+        var html = this.progressBar;
+        $(this.container).html(html);
+    };
+
+
     ProgressBar.prototype.update = function (value) {
+
+        this.$el = this.$container.find(this.id);
 
         var $div = this.$el.find('div');
 
@@ -44,7 +53,10 @@ define([
 
         $div.attr('aria-valuenow', value);
         $div.css('width', value + '%');
-        $span.text(value + '% Complete');
+
+        if(value !== 0) {
+            $span.text(' '+ i18nLabels.loading_in_progress + ' ... '+ value + '% '+i18nLabels.completed);
+        }
 
     };
 
