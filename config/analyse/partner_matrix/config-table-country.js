9f0e14b5076605c87a29d16e46d275e89bf2ecf7
tanzm <Tanzila.Mohammad@fao.org>
2016-09-08 13:37:44 +0100

    Resource Partner Matrix

---
+++ config/analyse/partner_matrix/config-table-country.js
@@ -0,0 +1,143 @@
+/*global define*/
+
+define(function () {
+
+    'use strict';
+
+    return {
+            dashboard: {
+                //default dataset id
+                uid: "adam_usd_commitment",
+
+                items: [
+                    {
+                        id: "partner-matrix-2",
+                        type: 'custom',
+                        config: {
+                            "groupedRow":true,
+                            "aggregationFn":{"value":"sum"},
+                            "formatter":"localstring",
+                            "decimals":2,
+                            "showRowHeaders":true,
+                            "columns":["indicator"],
+                            "rows":["recipientcode", "donorcode"],
+                            "aggregations":[],
+                            "values":["value"],
+                            inputFormat : "fenixtool"
+                        },
+
+                        filterFor: {
+                            "filter_total_ODA": ['year', 'oda', 'recipientcode']
+                        },
+
+                        postProcess: [
+                            {
+                                "name": "filter",
+                                "sid": [
+                                    {
+                                        "uid": "adam_usd_aggregation_table"
+                                    }
+                                ],
+                                "parameters": {
+                                    "columns": [
+                                        "recipientcode",
+                                        "donorcode",
+                                        "value",
+                                        "unitcode"
+                                    ],
+                                    "rows": {
+                                        "recipientcode": {
+                                            "codes": [
+                                                {
+                                                    "uid": "crs_recipients",
+                                                    "version": "2016",
+                                                    "codes": [
+                                                        "625"
+                                                    ]
+                                                }
+                                            ]
+                                        },
+                                        "oda": {
+                                            "enumeration": [
+                                                "usd_commitment"
+                                            ]
+                                        },
+                                        "year": {
+                                            "time": [
+                                                {
+                                                    "from": 2000,
+                                                    "to": 2014
+                                                }
+                                            ]
+                                        }
+                                    }
+                                },
+                                "rid":{"uid":"filter_total_ODA"}
+                            },
+                            {
+                                "name": "group",
+                                "parameters": {
+                                    "by": [
+                                        "recipientcode", "donorcode"
+                                    ],
+                                    "aggregations": [
+                                        {
+                                            "columns": [
+                                                "value"
+                                            ],
+                                            "rule": "SUM"
+                                        },
+                                        {
+                                            "columns": [
+                                                "unitcode"
+                                            ],
+                                            "rule": "first"
+                                        }
+                                    ]
+                                },
+                                "rid": {
+                                    "uid": "total_oda"
+                                }
+                            },
+                            {
+                                "name": "select",
+                                "parameters": {
+                                    "query": "WHERE donorcode NOT IN (?)", // skipping regional recipient countries (e.g. "Africa, regional"; "North of Sahara, regional")
+                                    "queryParameters": [
+                                        {"value": 'NA'}
+                                    ]
+                                }
+                            },
+                            {
+                                "name": "addcolumn",
+                                "parameters": {
+                                    "column": {
+                                        "dataType": "text",
+                                        "id": "indicator",
+                                        key:true,
+                                        "title": {
+                                            "EN": "Indicator"
+                                        },
+                                        "domain": {
+                                            "codes": [
+                                                {
+                                                    "extendedName": {
+                                                        "EN": "Adam Processes"
+                                                    },
+                                                    "idCodeList": "adam_processes"
+                                                }
+                                            ]
+                                        },
+                                        "subject": null
+                                    },
+                                    "value": "Total ODA (mil $)"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+       }
+
+
+});
---
+++ config/analyse/partner_matrix/config-table-donor.js
@@ -0,0 +1,143 @@
+/*global define*/
+
+define(function () {
+
+    'use strict';
+
+    return {
+            dashboard: {
+                //default dataset id
+                uid: "adam_usd_commitment",
+
+                items: [
+                    {
+                        id: "partner-matrix-2",
+                        type: 'custom',
+                        config: {
+                            "groupedRow":false,
+                            "aggregationFn":{"value":"sum"},
+                            "formatter":"localstring",
+                            "decimals":2,
+                            "showRowHeaders":true,
+                            "columns":["indicator"],
+                            "rows":["donorcode", "recipientcode"],
+                            "aggregations":[],
+                            "values":["value"],
+                            inputFormat : "fenixtool"
+                        },
+
+                        filterFor: {
+                            "filter_total_ODA": ['year', 'oda', 'donorcode']
+                        },
+
+                        postProcess: [
+                            {
+                                "name": "filter",
+                                "sid": [
+                                    {
+                                        "uid": "adam_usd_aggregation_table"
+                                    }
+                                ],
+                                "parameters": {
+                                    "columns": [
+                                        "donorcode",
+                                        "recipientcode",
+                                        "value",
+                                        "unitcode"
+                                    ],
+                                    "rows": {
+                                        "donorcode": {
+                                            "codes": [
+                                                {
+                                                    "uid": "crs_donors",
+                                                    "version": "2016",
+                                                    "codes": [
+                                                        "1"
+                                                    ]
+                                                }
+                                            ]
+                                        },
+                                        "oda": {
+                                            "enumeration": [
+                                                "usd_commitment"
+                                            ]
+                                        },
+                                        "year": {
+                                            "time": [
+                                                {
+                                                    "from": 2000,
+                                                    "to": 2014
+                                                }
+                                            ]
+                                        }
+                                    }
+                                },
+                                "rid":{"uid":"filter_total_ODA"}
+                            },
+                            {
+                                "name": "group",
+                                "parameters": {
+                                    "by": [
+                                        "donorcode",  "recipientcode"
+                                    ],
+                                    "aggregations": [
+                                        {
+                                            "columns": [
+                                                "value"
+                                            ],
+                                            "rule": "SUM"
+                                        },
+                                        {
+                                            "columns": [
+                                                "unitcode"
+                                            ],
+                                            "rule": "first"
+                                        }
+                                    ]
+                                },
+                                "rid": {
+                                    "uid": "total_oda"
+                                }
+                            },
+                            {
+                                "name": "select",
+                                "parameters": {
+                                    "query": "WHERE recipientcode NOT IN (?)", // skipping regional recipient countries (e.g. "Africa, regional"; "North of Sahara, regional")
+                                    "queryParameters": [
+                                        {"value": 'NA'}
+                                    ]
+                                }
+                            },
+                            {
+                                "name": "addcolumn",
+                                "parameters": {
+                                    "column": {
+                                        "dataType": "text",
+                                        "id": "indicator",
+                                        key:true,
+                                        "title": {
+                                            "EN": "Indicator"
+                                        },
+                                        "domain": {
+                                            "codes": [
+                                                {
+                                                    "extendedName": {
+                                                        "EN": "Adam Processes"
+                                                    },
+                                                    "idCodeList": "adam_processes"
+                                                }
+                                            ]
+                                        },
+                                        "subject": null
+                                    },
+                                    "value": "Total ODA (mil $)"
+                                }
+                            }
+                        ]
+                    },
+                ]
+            }
+       }
+
+
+});
