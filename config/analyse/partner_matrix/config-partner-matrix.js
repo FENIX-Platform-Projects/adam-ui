1f63554ec9af61bcf0e5cd0696a2a6ff382b032b
tanzm <Tanzila.Mohammad@fao.org>
2016-09-16 14:04:26 +0100

    Resource Partner Matrix

--- config/analyse/partner_matrix/config-partner-matrix.js
+++ config/analyse/partner_matrix/config-partner-matrix.js
@@ -7,19 +7,6 @@ define(function ( ) {
         dashboard: {
             DEFAULT_TOPIC : 'country' //country, donor or country-donor
         },
-        filter: {
-            RECIPIENT_COUNTRY: 'recipientcode',
-            RESOURCE_PARTNER: 'donorcode',
-            SECTOR: 'parentsector_code',
-            SUB_SECTOR: 'purposecode',
-            CHANNELS_SUBCATEGORY: 'channelsubcategory_code',
-            CHANNEL: 'channelcode',
-            ODA: 'oda',
-            YEAR: 'year',
-            YEAR_FROM: 'year-from',
-            YEAR_TO: 'year-to',
-            COUNTRY: 'countrycode'
-        },
         topic: {
             SELECTED_TOPIC: 'selected_topic',
             RECIPIENT_COUNTRY_SELECTED: 'country',
