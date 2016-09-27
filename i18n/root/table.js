91bfb2ab95ba5d3215eaf8739c5ac6a861b2e8c6
tanzm <Tanzila.Mohammad@fao.org>
2016-09-27 10:35:44 +0100

    FAO Comparative advantage updates

--- i18n/root/table.js
+++ i18n/root/table.js
@@ -1,8 +1,5 @@
 /*global define*/
 define({
     "results": "Results",
-    "rowsFound": "Rows Found",
-    "comp_advantage_desc": "The FAO Comparative Advantage is an objective output based on a calculation which uses data as it is reported by resource partners to the OECD-DAC.",
-    "read_more": "The details of the calculations performed are available",
-    "here": "here"
+    "rowsFound": "Rows Found"
 });
