db64b624e82ba83246fff33d0bc28840f0b759ad
tanzm <Tanzila.Mohammad@fao.org>
2016-09-29 10:44:55 +0100

    Partner profiles updates

--- config/profiles/lateral-menu-config.js
+++ config/profiles/lateral-menu-config.js
@@ -4,84 +4,52 @@ define({
     "multiple": false,
     "data": [
       {
-        "id": "demographic",
-        "state": {
-          "opened": true
-        },
-        "children": [
-          {
-            "id": "population"
-          },
-          {
-            "id": "education"
-          },
-          {
-            "id": "health"
-          },
-          {
-            "id": "labour"
-          }
-        ]
+        "id": "1"
       },
       {
-        "id": "economics",
-        "state": {
-          "opened": true
-        },
-        "children": [
-          {
-            "id": "balance_of_payments"
-          },
-          {
-            "id": "gdp"
-          },
-          {
-            "id": "monetary_statistics",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "financial_flows",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "public_finance"
-          },
-          {
-            "id": "debt",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "inflation",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "energy",
-            "state": {"disabled": true}
-
-          },
-          {
-            "id": "tourism",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "infrastructure",
-            "state": {"disabled": true}
-          },
-          {
-            "id": "production",
-            "children": [
-              {
-                "id": "agriculture_production",
-                "state": {"disabled": true}
-              },
-              {
-                "id": "mining_production",
-                "state": {"disabled": true}
-              }
-            ],
-            "state": {"disabled": true}
-          }
-        ]
+        "id": "2",
+      },
+      {
+        "id": "3",
+      },
+      {
+        "id": "4",
+      },
+      {
+        "id": "5",
+      },
+      {
+        "id": "6",
+      },
+      {
+        "id": "7",
+      },
+      {
+        "id": "8",
+      },
+      {
+        "id": "9",
+      },
+      {
+        "id": "10",
+      },
+      {
+        "id": "11",
+      },
+      {
+        "id": "12",
+      },
+      {
+        "id": "13",
+      },
+      {
+        "id": "14",
+      },
+      {
+        "id": "15",
+      },
+      {
+        "id": "16"
       }
     ]
   },
