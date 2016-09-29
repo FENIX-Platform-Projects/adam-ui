bef88c4c84d6b313c409b2aa0705e9cef8d109ee
tanzm <Tanzila.Mohammad@fao.org>
2016-09-29 10:45:22 +0100

    Partner profiles updates

---
+++ i18n/root/profiles.js
@@ -0,0 +1,22 @@
+/*global define*/
+define({
+    "not_available" : "Profile not available",
+
+    //Lateral menu labels
+    "menu_1" : "Demographic and social statistics",
+    "menu_2" : "Population",
+    "menu_3" : "Education",
+    "menu_4" : "Health",
+    "menu_5" : "Labour",
+    "menu_6" : "Economic statistics",
+    "menu_7" : "Balance of Payments",
+    "menu_8" : "GDP",
+    "menu_9" : "Monetary Statistics",
+    "menu_10" : "Financial Flows",
+    "menu_11" : "Public Finance",
+    "menu_12" : "Debt",
+    "menu_13" : "Inflation",
+    "menu_14" : "Energy",
+    "menu_15" : "Tourism",
+    "menu_16" : "Infrastructure"
+});
